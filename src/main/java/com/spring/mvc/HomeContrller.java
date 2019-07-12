f5e2be0c225261b7a96fb31c95e4d996c706d8c3
yuvadany <toyuvaraj@gmail.com>
2019-07-12 15:27:39 +0530

    logback

--- src/main/java/com/spring/mvc/HomeContrller.java
+++ src/main/java/com/spring/mvc/HomeContrller.java
@@ -1,5 +1,7 @@
 package com.spring.mvc;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,16 +11,20 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeContrller {
 
-	@GetMapping ("/")
-	public ModelAndView home()
-	{
+	private final Logger logger = LoggerFactory.getLogger(HomeContrller.class);
+
+	@GetMapping("/")
+	public ModelAndView home() {
 		ModelAndView mv = new ModelAndView("login");
+		logger.info(" home() function");
 		return mv;
 	}
+
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
-	public ModelAndView  welcome() {
-		ModelAndView  modelView = new ModelAndView ("home");
+	public ModelAndView welcome() {
+		ModelAndView modelView = new ModelAndView("home");
 		modelView.addObject("name", "Yuvi");
+		logger.info(" welcome() function");
 		return modelView;
 	}
 }
